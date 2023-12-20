e44134f1f63bdad0bba186174f4855119080df56
totruong <totruong591@gmail.com>
2023-12-20 14:01:20 +0700

    n2 shoptobi

--- shop-tobi/src/index.js
+++ shop-tobi/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homePage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Shop Tobi</h1>
+  <HomePage />
 );
 
 // If you want to start measuring performance in your app, pass a function
